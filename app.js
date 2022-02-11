58b82bcc7dab046446d80ccf2f7335a21200ab54
ubuntor <2906473+ubuntor@users.noreply.github.com>
2022-02-11 01:16:05 -0500

    field drawing

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const WIDTH_ORIG = 960;
+const HEIGHT_ORIG = 600;
 const N = 4;
 const ALPHA = 1;
 const BETA = 1+2/N;
@@ -6,6 +8,8 @@ const DELTA = 1 - 1/N;
 const NUM_MOVES = 75;
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
+let W = WIDTH_ORIG * devicePixelRatio;
+let H = HEIGHT_ORIG * devicePixelRatio;
 
 function f(a) {
     let [x,y,z,w] = a;
@@ -146,24 +150,134 @@ function get_moves(f, x0_init) {
 }
 
 function resize() {
-    canvas.width = 800; //window.innerWidth;
-    canvas.height = 600; //window.innerHeight;    
+    canvas.width = W;
+    canvas.height = H;
+    canvas.style.width = WIDTH_ORIG + 'px';
+    canvas.style.height = HEIGHT_ORIG + 'px';
 }
 
-function render() {
+function rounded_rect(x, y, w, h, r) {
+    ctx.moveTo(x+r, y);
+    ctx.arcTo(x+w, y,   x+w, y+h, r);
+    ctx.arcTo(x+w, y+h, x,   y+h, r);
+    ctx.arcTo(x,   y+h, x,   y,   r);
+    ctx.arcTo(x,   y,   x+w, y,   r);
+}
+
+function render(timestamp) {
+    // TODO: use timestamp
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.rect(30, 50, 8, 500);
-    ctx.rect(50, 50, 700, 500);
+    ctx.fillStyle = '#f23af2';
     ctx.strokeStyle = '#f23af2';
-    ctx.shadowColor = '#ffffff';
-    ctx.shadowBlur = 2;
+    ctx.shadowColor = '#f23af2';
+    ctx.lineWidth=1;
+    ctx.shadowBlur=2;
+    ctx.beginPath();
+    ctx.rect(35-0.5, 100-0.5, 8, H-200);
+    ctx.rect(50-0.5, 100-0.5, 300, H-200);
     ctx.stroke();
+    ctx.strokeStyle = '#f23af255';
+    ctx.beginPath();
+    ctx.rect(150-0.5, 100-0.5, 100, H-200);
+    ctx.stroke();
+    ctx.strokeStyle = '#f23af2bb';
+    ctx.beginPath();
+    rounded_rect(50+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
+    rounded_rect(150+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
+    rounded_rect(250+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
+    ctx.stroke();
+
+    // expand receptor
+    ctx.beginPath();
+    ctx.moveTo(100+15-10-0.5, H-150-0.5);
+    ctx.lineTo(100+30-10-0.5, H-150-0.5);
+    ctx.lineTo(100+30-10-0.5, H-165+5-0.5);
+    ctx.lineTo(150-5-10-0.5, H-142.5-0.5);
+    ctx.lineTo(100+30-10-0.5, H-120-5-0.5);
+    ctx.lineTo(100+30-10-0.5, H-135-0.5);
+    ctx.lineTo(100+15-10-0.5, H-135-0.5);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(100-15+10-0.5, H-150-0.5);
+    ctx.lineTo(100-30+10-0.5, H-150-0.5);
+    ctx.lineTo(100-30+10-0.5, H-165+5-0.5);
+    ctx.lineTo(50+5+10-0.5, H-142.5-0.5);
+    ctx.lineTo(100-30+10-0.5, H-120-5-0.5);
+    ctx.lineTo(100-30+10-0.5, H-135-0.5);
+    ctx.lineTo(100-15+10-0.5, H-135-0.5);
+    ctx.closePath();
+    ctx.stroke();
+
+    // reflect receptor
+    ctx.beginPath();
+    ctx.moveTo(200-0.5, H-165-0.5);
+    ctx.lineTo(200-0.5, H-120-0.5);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(150+15-0.5, H-165+7-0.5);
+    ctx.lineTo(200-10-0.5, H-142.5-0.5);
+    ctx.lineTo(150+15-0.5, H-120-7-0.5);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(250-15-0.5, H-165+7-0.5);
+    ctx.lineTo(200+10-0.5, H-142.5-0.5);
+    ctx.lineTo(250-15-0.5, H-120-7-0.5);
+    ctx.closePath();
+    ctx.stroke();
+
+    // contract receptor
+    ctx.beginPath();
+    ctx.moveTo(250+15-0.5, H-150-0.5);
+    ctx.lineTo(250+30-0.5, H-150-0.5);
+    ctx.lineTo(250+30-0.5, H-165+5-0.5);
+    ctx.lineTo(300-5-0.5, H-142.5-0.5);
+    ctx.lineTo(250+30-0.5, H-120-5-0.5);
+    ctx.lineTo(250+30-0.5, H-135-0.5);
+    ctx.lineTo(250+15-0.5, H-135-0.5);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(350-15-0.5, H-150-0.5);
+    ctx.lineTo(350-30-0.5, H-150-0.5);
+    ctx.lineTo(350-30-0.5, H-165+5-0.5);
+    ctx.lineTo(300+5-0.5, H-142.5-0.5);
+    ctx.lineTo(350-30-0.5, H-120-5-0.5);
+    ctx.lineTo(350-30-0.5, H-135-0.5);
+    ctx.lineTo(350-15-0.5, H-135-0.5);
+    ctx.closePath();
+    ctx.stroke();
+
+
+    ctx.shadowBlur = 0;
+    ctx.textAlign = 'left';
+    ctx.font = '12pt Covenant';
+    ctx.save();
+    ctx.rotate(-Math.PI/2);
+    ctx.fillText('INTENSITY', 100-H, 30);
+    ctx.restore();
+    ctx.textAlign = 'center';
+    ctx.fillText('EXPAND', 100, H-75);
+    ctx.fillText('REFLECT', 200, H-75);
+    ctx.fillText('CONTRACT', 300, H-75);
+    ctx.font = '48pt Covenant';
+    ctx.fillText('Z', 100, H-20);
+    ctx.fillText('X', 200, H-20);
+    ctx.fillText('C', 300, H-20);
     window.requestAnimationFrame(render);
 }
 
-function main() {
+function onKeyDown(e) {
+
+}
+
+async function main() {
+    const font = new FontFace('Covenant', 'url(Covenant5x5.ttf)');
+    await font.load();
+    document.fonts.add(font);
+    document.addEventListener("keydown", onKeyDown, false);
     let [x0, moves] = get_moves(f, [-0.5,-1,-2,-2]);
     console.log(x0, moves);
     resize();
