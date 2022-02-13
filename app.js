318d37a0a4482a7825b0e5ae52c62bb4a73cbd26
ubuntor <2906473+ubuntor@users.noreply.github.com>
2022-02-13 17:59:42 -0500

    phaser rewrite

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 const WIDTH_ORIG = 960;
-const HEIGHT_ORIG = 600;
+const HEIGHT_ORIG = 720;
 const N = 4;
 const ALPHA = 1;
-const BETA = 1+2/N;
-const GAMMA = 0.75 - 1/(2*N);
-const DELTA = 1 - 1/N;
+const BETA = 1 + 2 / N;
+const GAMMA = 0.75 - 1 / (2 * N);
+const DELTA = 1 - 1 / N;
 const NUM_MOVES = 75;
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
 let W = WIDTH_ORIG * devicePixelRatio;
 let H = HEIGHT_ORIG * devicePixelRatio;
 
-function f(a) {
-    let [x,y,z,w] = a;
-    // McCormick + Styblinski-Tang: (-0.547, -1.547, -2.904, -2.904)
-    return (Math.sin(x+y) + (x-y)**2 - 1.5*x + 2.5*y + 1) + ((z**4 - 16*z**2 + 5*z + w**4 - 16*w**2 + 5*w)/2);
+function balanced_ternary(x, precision) {
+    var x = floor(x * pow(3, precision));
+    let digits = [];
+    while (x > 0) {
+        let digit = [0, 1, -1][x % 3];
+        digits.push(digit);
+        x -= digit;
+        x /= 3;
+    }
+    return digits;
 }
 
-function vec_add(a,b) {
-    return a.map((e,i) => e+b[i]);
+function f(a) {
+    let [x, y, z, w] = a;
+    // McCormick + Styblinski-Tang: (-0.547, -1.547, -2.904, -2.904)
+    return (Math.sin(x + y) + (x - y) ** 2 - 1.5 * x + 2.5 * y + 1) + ((z ** 4 - 16 * z ** 2 + 5 * z + w ** 4 - 16 * w ** 2 + 5 * w) / 2);
 }
-function vec_sub(a,b) {
-    return a.map((e,i) => e-b[i]);
+
+function vec_add(a, b) {
+    return a.map((e, i) => e + b[i]);
 }
-function scal_mul(v,a) {
-    return a.map((e) => v*e);
+function vec_sub(a, b) {
+    return a.map((e, i) => e - b[i]);
+}
+function scal_mul(v, a) {
+    return a.map((e) => v * e);
 }
 
 function simplex_replace(simplex, v) {
     simplex.pop()
     simplex.push(v);
-    simplex.sort((a,b) => a[0]-b[0]);
+    simplex.sort((a, b) => a[0] - b[0]);
 }
 
 function make_simplex(x0) {
@@ -41,7 +51,7 @@ function make_simplex(x0) {
         simplex.push(x);
     }
     simplex = simplex.map((e) => [f(e), e]);
-    simplex.sort((a,b) => a[0]-b[0]);
+    simplex.sort((a, b) => a[0] - b[0]);
     return simplex;
 }
 
@@ -51,7 +61,7 @@ function get_moves(f, x0_init) {
     let best_x0 = [];
 
     for (let trial = 0; trial < 100; trial++) {
-        let x0 = x0_init.map((e) => e + 0.01*(Math.random()*2-1));
+        let x0 = x0_init.map((e) => e + 0.01 * (Math.random() * 2 - 1));
         let simplex = make_simplex(x0);
         let moves = [];
         let prev_move = -1;
@@ -62,10 +72,10 @@ function get_moves(f, x0_init) {
             for (let i = 0; i < N; i++) {
                 xc = vec_add(xc, simplex[i][1]);
             }
-            xc = scal_mul(1/N, xc);
-            let [f1,x1] = simplex[0];
-            let [fn,xn] = simplex[N-1];
-            let [fn1,xn1] = simplex[N];
+            xc = scal_mul(1 / N, xc);
+            let [f1, x1] = simplex[0];
+            let [fn, xn] = simplex[N - 1];
+            let [fn1, xn1] = simplex[N];
             let xr = vec_add(xc, scal_mul(ALPHA, vec_sub(xc, xn1)));
             let fr = f(xr);
             if (f1 <= fr && fr < fn) {
@@ -94,12 +104,12 @@ function get_moves(f, x0_init) {
                 } else {
                     //console.log("..C shrink");
                     cur_move = 2;
-                    for (let i = 1; i < N+1; i++) {
+                    for (let i = 1; i < N + 1; i++) {
                         let x = vec_add(x1, scal_mul(DELTA, vec_sub(simplex[i][1], x1)));
                         let fx = f(x);
-                        simplex[i] = [fx,x];
+                        simplex[i] = [fx, x];
                     }
-                    simplex.sort((a,b) => a[0]-b[0]);
+                    simplex.sort((a, b) => a[0] - b[0]);
                 }
             } else { // fr >= fn1
                 xic = vec_sub(xc, scal_mul(GAMMA, vec_sub(xr, xc)))
@@ -111,19 +121,19 @@ function get_moves(f, x0_init) {
                 } else {
                     //console.log("..C shrink");
                     cur_move = 2;
-                    for (let i = 1; i < N+1; i++) {
+                    for (let i = 1; i < N + 1; i++) {
                         let x = vec_add(x1, scal_mul(DELTA, vec_sub(simplex[i][1], x1)));
                         let fx = f(x);
-                        simplex[i] = [fx,x];
+                        simplex[i] = [fx, x];
                     }
-                    simplex.sort((a,b) => a[0]-b[0]);
+                    simplex.sort((a, b) => a[0] - b[0]);
                 }
             }
             if (cur_move != prev_move) {
                 if (prev_move != -1) {
-                    let num_split = Math.floor(Math.log(cur_streak)/Math.log(3))+1;
-                    for (let i = 0; i < num_split-1; i++) {
-                        let mult = Math.floor(cur_streak/(num_split-i)); // TODO: add jitter
+                    let num_split = Math.floor(Math.log(cur_streak) / Math.log(3)) + 1;
+                    for (let i = 0; i < num_split - 1; i++) {
+                        let mult = Math.floor(cur_streak / (num_split - i)); // TODO: add jitter
                         moves.push([mult, prev_move]);
                         cur_streak -= mult;
                     }
@@ -134,13 +144,13 @@ function get_moves(f, x0_init) {
             }
             cur_streak++;
         }
-        let counts = [0,0,0];
-        for (let [_,x] of moves) {
+        let counts = [0, 0, 0];
+        for (let [_, x] of moves) {
             counts[x]++;
         }
-        let score = (Math.max(...counts) - Math.min(...counts))/moves.length;
+        let score = (Math.max(...counts) - Math.min(...counts)) / moves.length;
         if (score < best_score) {
-            console.log(trial, score)
+            //console.log(trial, score);
             best_x0 = x0;
             best_score = score;
             best_moves = moves;
@@ -149,139 +159,103 @@ function get_moves(f, x0_init) {
     return [best_x0, best_moves];
 }
 
-function resize() {
-    canvas.width = W;
-    canvas.height = H;
-    canvas.style.width = WIDTH_ORIG + 'px';
-    canvas.style.height = HEIGHT_ORIG + 'px';
+class Play extends Phaser.Scene {
+    preload() {
+        this.load.plugin('rexroundrectangleplugin', 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/8b4ad8083e48019f39b37d8f629379851ca8ec13/dist/rexroundrectangleplugin.min.js', true);
+    }
+
+    create() {
+        this.cameras.main.setBackgroundColor("#111111");
+        this.graphics = this.add.graphics();
+        this.add.rectangle(50 - 0.5, 100 - 0.5, 300, H - 200).setStrokeStyle(1, 0xf23af2).setOrigin(0);
+        this.add.rectangle(50 - 0.5, 5 - 0.5, 300, 100 - 5).setStrokeStyle(1, 0xf23af2).setOrigin(0);
+        this.add.rectangle(150 - 0.5, 100 - 0.5, 100, H - 200).setStrokeStyle(1, 0xf23af2, 0.33).setOrigin(0);
+        this.add.text(15, H - 100, 'INTENSITY', { font: '12pt Covenant', fill: '#f23af2' }).setAngle(-90);
+        this.add.rectangle(35 - 0.5, 100 - 0.5, 8, H - 200).setStrokeStyle(1, 0xf23af2).setOrigin(0);
+        this.add.rectangle(357 - 0.5, 600 - 0.5, 8, H - 700).setStrokeStyle(1, 0xf23af2).setOrigin(0);
+        this.add.text(370, 10, 'OBJECTIVE FUNCTION', { font: '12pt Covenant', fill: '#f23af2' });
+        this.add.text(370, 80, 'TRANSFORMS', { font: '12pt Covenant', fill: '#f23af2' });
+        this.add.text(370, 150, 'POLYTOPE', { font: '12pt Covenant', fill: '#f23af2' });
+        this.add.text(W - 200, 150, 'ALGORITHM', { font: '12pt Covenant', fill: '#f23af2' });
+        this.add.text(400, H - 110, 'SCORE', { font: '12pt Covenant', fill: '#f23af2' });
+        this.add.text(100, H - 85, 'EXPAND', { font: '12pt Covenant', fill: '#f23af2' }).setOrigin(0.5);
+        this.add.text(200, H - 85, 'REFLECT', { font: '12pt Covenant', fill: '#f23af2' }).setOrigin(0.5);
+        this.add.text(300, H - 85, 'CONTRACT', { font: '12pt Covenant', fill: '#f23af2' }).setOrigin(0.5);
+        this.add.text(100, H - 65, 'Z', { font: '72pt Thaleah', fill: '#f23af2' }).setOrigin(0.5);
+        this.add.text(200, H - 65, 'X', { font: '72pt Thaleah', fill: '#f23af2' }).setOrigin(0.5);
+        this.add.text(300, H - 65, 'C', { font: '72pt Thaleah', fill: '#f23af2' }).setOrigin(0.5);
+        let expand_receptor = this.add.container(100 - 0.5, H - 140 - 0.5);
+        expand_receptor.add(this.add.rexRoundRectangle(0, 0, 100 - 2 * 5, 70, 10).setStrokeStyle(2, 0xf23af2, 0.75));
+        expand_receptor.add(this.add.polygon(0, 0, [[5, 8], [20, 8], [20, 15], [40, 0], [20, -15], [20, -8], [5, -8]]).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+        expand_receptor.add(this.add.polygon(0, 0, [[-5, -8], [-20, -8], [-20, -15], [-40, -0], [-20, 15], [-20, 8], [-5, 8]]).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+
+        let reflect_receptor = this.add.container(200 - 0.5, H - 140 - 0.5);
+        reflect_receptor.add(this.add.rexRoundRectangle(0, 0, 100 - 2 * 5, 70, 10).setStrokeStyle(2, 0xf23af2, 0.75));
+        reflect_receptor.add(this.add.line(0, 0, 0, 25, 0, -25).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+        reflect_receptor.add(this.add.triangle(0, 0, 7, 0, 35, 18, 35, -18).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+        reflect_receptor.add(this.add.triangle(0, 0, -7, 0, -35, 18, -35, -18).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+
+        let contract_receptor = this.add.container(300 - 0.5, H - 140 - 0.5);
+        contract_receptor.add(this.add.rexRoundRectangle(0, 0, 100 - 2 * 5, 70, 10).setStrokeStyle(2, 0xf23af2, 0.75));
+        contract_receptor.add(this.add.polygon(0, 0, [[-40 + 2, -8], [-25 + 2, -8], [-25 + 2, -15], [-5 + 2, -0], [-25 + 2, 15], [-25 + 2, 8], [-40 + 2, 8]]).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+        contract_receptor.add(this.add.polygon(0, 0, [[40 - 2, 8], [25 - 2, 8], [25 - 2, 15], [5 - 2, 0], [25 - 2, -15], [25 - 2, -8], [40 - 2, -8]]).setStrokeStyle(2, 0xf23af2, 0.75).setOrigin(0, 0));
+
+        this.receptors = [expand_receptor, reflect_receptor, contract_receptor];
+
+        for (let receptor of this.receptors) {
+            receptor.setScale(0.9);
+            this.tweens.add({
+                targets: receptor,
+                scale: 1,
+                loop: -1,
+                duration: 500,
+                ease: 'Sine.easeOut',
+            });
+        }
+
+        this.tmp = this.add.text(500, 500, 'test');
+        let [x0, moves] = get_moves(f, [-0.5, -1, -2, -2]);
+        this.moves = moves;
+        this.x0 = x0;
+        //console.log(this.x0, this.moves);
+    }
+
+    update() {
+        this.graphics.clear();
+        //this.graphics.lineStyle(1, 0xf23af2, 0.75);
+        let debug = [];
+        debug = debug.concat(this.x0);
+        for (let i = 0; i < 10; i++) {
+            let x = this.moves[i][1];
+            debug.push(`${'ZXC'[this.moves[i][1]]} ${this.moves[i][0]}`);
+            this.graphics.fillStyle([0xdd0000, 0x00dd00, 0x0000dd][x]);
+            this.graphics.fillRoundedRect(55 - 0.5 + 100 * x, H - 175 - 0.5 - i * 90, 90, 70, 10);
+        }
+        this.tmp.setText(debug);
+    }
 }
 
-function rounded_rect(x, y, w, h, r) {
-    ctx.moveTo(x+r, y);
-    ctx.arcTo(x+w, y,   x+w, y+h, r);
-    ctx.arcTo(x+w, y+h, x,   y+h, r);
-    ctx.arcTo(x,   y+h, x,   y,   r);
-    ctx.arcTo(x,   y,   x+w, y,   r);
-}
-
-function render(timestamp) {
-    // TODO: use timestamp
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = '#f23af2';
-    ctx.strokeStyle = '#f23af2';
-    ctx.shadowColor = '#f23af2';
-    ctx.lineWidth=1;
-    ctx.shadowBlur=2;
-    ctx.beginPath();
-    ctx.rect(35-0.5, 100-0.5, 8, H-200);
-    ctx.rect(50-0.5, 100-0.5, 300, H-200);
-    ctx.stroke();
-    ctx.strokeStyle = '#f23af255';
-    ctx.beginPath();
-    ctx.rect(150-0.5, 100-0.5, 100, H-200);
-    ctx.stroke();
-    ctx.strokeStyle = '#f23af2bb';
-    ctx.beginPath();
-    rounded_rect(50+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
-    rounded_rect(150+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
-    rounded_rect(250+5-0.5, H-175-5-0.5, 100-2*5, 75, 10);
-    ctx.stroke();
-
-    // expand receptor
-    ctx.beginPath();
-    ctx.moveTo(100+15-10-0.5, H-150-0.5);
-    ctx.lineTo(100+30-10-0.5, H-150-0.5);
-    ctx.lineTo(100+30-10-0.5, H-165+5-0.5);
-    ctx.lineTo(150-5-10-0.5, H-142.5-0.5);
-    ctx.lineTo(100+30-10-0.5, H-120-5-0.5);
-    ctx.lineTo(100+30-10-0.5, H-135-0.5);
-    ctx.lineTo(100+15-10-0.5, H-135-0.5);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(100-15+10-0.5, H-150-0.5);
-    ctx.lineTo(100-30+10-0.5, H-150-0.5);
-    ctx.lineTo(100-30+10-0.5, H-165+5-0.5);
-    ctx.lineTo(50+5+10-0.5, H-142.5-0.5);
-    ctx.lineTo(100-30+10-0.5, H-120-5-0.5);
-    ctx.lineTo(100-30+10-0.5, H-135-0.5);
-    ctx.lineTo(100-15+10-0.5, H-135-0.5);
-    ctx.closePath();
-    ctx.stroke();
-
-    // reflect receptor
-    ctx.beginPath();
-    ctx.moveTo(200-0.5, H-165-0.5);
-    ctx.lineTo(200-0.5, H-120-0.5);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(150+15-0.5, H-165+7-0.5);
-    ctx.lineTo(200-10-0.5, H-142.5-0.5);
-    ctx.lineTo(150+15-0.5, H-120-7-0.5);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(250-15-0.5, H-165+7-0.5);
-    ctx.lineTo(200+10-0.5, H-142.5-0.5);
-    ctx.lineTo(250-15-0.5, H-120-7-0.5);
-    ctx.closePath();
-    ctx.stroke();
-
-    // contract receptor
-    ctx.beginPath();
-    ctx.moveTo(250+15-0.5, H-150-0.5);
-    ctx.lineTo(250+30-0.5, H-150-0.5);
-    ctx.lineTo(250+30-0.5, H-165+5-0.5);
-    ctx.lineTo(300-5-0.5, H-142.5-0.5);
-    ctx.lineTo(250+30-0.5, H-120-5-0.5);
-    ctx.lineTo(250+30-0.5, H-135-0.5);
-    ctx.lineTo(250+15-0.5, H-135-0.5);
-    ctx.closePath();
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(350-15-0.5, H-150-0.5);
-    ctx.lineTo(350-30-0.5, H-150-0.5);
-    ctx.lineTo(350-30-0.5, H-165+5-0.5);
-    ctx.lineTo(300+5-0.5, H-142.5-0.5);
-    ctx.lineTo(350-30-0.5, H-120-5-0.5);
-    ctx.lineTo(350-30-0.5, H-135-0.5);
-    ctx.lineTo(350-15-0.5, H-135-0.5);
-    ctx.closePath();
-    ctx.stroke();
-
-
-    ctx.shadowBlur = 0;
-    ctx.textAlign = 'left';
-    ctx.font = '12pt Covenant';
-    ctx.save();
-    ctx.rotate(-Math.PI/2);
-    ctx.fillText('INTENSITY', 100-H, 30);
-    ctx.restore();
-    ctx.textAlign = 'center';
-    ctx.fillText('EXPAND', 100, H-75);
-    ctx.fillText('REFLECT', 200, H-75);
-    ctx.fillText('CONTRACT', 300, H-75);
-    ctx.font = '48pt Covenant';
-    ctx.fillText('Z', 100, H-20);
-    ctx.fillText('X', 200, H-20);
-    ctx.fillText('C', 300, H-20);
-    window.requestAnimationFrame(render);
-}
-
-function onKeyDown(e) {
-
+async function add_font(name, url) {
+    const font = new FontFace(name, url);
+    await font.load();
+    document.fonts.add(font);
 }
 
 async function main() {
-    const font = new FontFace('Covenant', 'url(Covenant5x5.ttf)');
-    await font.load();
-    document.fonts.add(font);
-    document.addEventListener("keydown", onKeyDown, false);
-    let [x0, moves] = get_moves(f, [-0.5,-1,-2,-2]);
-    console.log(x0, moves);
-    resize();
-    window.requestAnimationFrame(render);
+    await add_font('Covenant', 'url(Covenant5x5.ttf)');
+    await add_font('Thaleah', 'url(ThaleahFat.ttf)');
+
+    const config = {
+        type: Phaser.AUTO,
+        width: W,
+        height: H,
+        zoom: 1 / window.devicePixelRatio,
+        scene: Play,
+        /*scale: {
+            autoCenter: Phaser.Scale.CENTER_BOTH
+        }*/
+    };
+    const game = new Phaser.Game(config);
 }
 
 main();
