696eb4fc5b0e2c87f34e27ef727906464b5d2c86
ubuntor <2906473+ubuntor@users.noreply.github.com>
2022-02-27 13:26:54 -0500

    even wobblier blobs (+modified normals!)

--- src/app.js
+++ src/app.js
@@ -170,7 +170,7 @@ function balanced_ternary(x, precision) {
 
 function f(a) {
     let [x, y, z, w] = a;
-    // -McCormick - Styblinski-Tang: (-0.547, -1.547, -2.904, -2.904)
+    // - McCormick - Styblinski-Tang: (-0.547, -1.547, -2.904, -2.904)
     // + gaussian noise: mean 0, stdev 0.0001
     return (0.0001 * (Math.sqrt(-2 * Math.log(1 - Math.random())) * Math.cos(2 * Math.PI * Math.random()))) - (Math.sin(x + y) + (x - y) ** 2 - 1.5 * x + 2.5 * y + 1) - ((z ** 4 - 16 * z ** 2 + 5 * z + w ** 4 - 16 * w ** 2 + 5 * w) / 2);
 }
@@ -1187,113 +1187,65 @@ class Play extends Phaser.Scene {
         tet_geom.computeVertexNormals();
         console.log(tet_geom);
 
-        // modified from https://codepen.io/marioecg/pen/abmXKvW
+        // heavily modified from https://codepen.io/marioecg/pen/abmXKvW
+        // and https://stegu.github.io/psrdnoise/3d-gallery/wobblysphere-threejs.html
+        // also tweaking to get rid of the period param
         const noise = `
-        // GLSL textureless classic 3D noise "cnoise",
-        // with an RSL-style periodic variant "pnoise".
-        // Author:  Stefan Gustavson (stefan.gustavson@liu.se)
-        // Version: 2011-10-11
-        //
-        // Many thanks to Ian McEwan of Ashima Arts for the
-        // ideas for permutation and gradient selection.
-        //
-        // Copyright (c) 2011 Stefan Gustavson. All rights reserved.
-        // Distributed under the MIT license. See LICENSE file.
-        // https://github.com/ashima/webgl-noise
-        //
-      
-        vec3 mod289(vec3 x)
-        {
-          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        // psrdnoise (c) Stefan Gustavson and Ian McEwan,
+        // ver. 2021-12-02, published under the MIT license:
+        // https://github.com/stegu/psrdnoise/
+
+        vec4 permute(vec4 i) {
+            vec4 im = mod(i, 289.0);
+            return mod(((im*34.0)+10.0)*im, 289.0);
         }
-      
-        vec4 mod289(vec4 x)
+
+        float psrdnoise(vec3 x, float alpha, out vec3 gradient)
         {
-          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        const mat3 M = mat3(0.0, 1.0, 1.0, 1.0, 0.0, 1.0,  1.0, 1.0, 0.0);
+        const mat3 Mi = mat3(-0.5, 0.5, 0.5, 0.5,-0.5, 0.5, 0.5, 0.5,-0.5);
+        vec3 uvw = M * x;
+        vec3 i0 = floor(uvw), f0 = fract(uvw);
+        vec3 g_ = step(f0.xyx, f0.yzz), l_ = 1.0 - g_;
+        vec3 g = vec3(l_.z, g_.xy), l = vec3(l_.xy, g_.z);
+        vec3 o1 = min( g, l ), o2 = max( g, l );
+        vec3 i1 = i0 + o1, i2 = i0 + o2, i3 = i0 + vec3(1.0);
+        vec3 v0 = Mi * i0, v1 = Mi * i1, v2 = Mi * i2, v3 = Mi * i3;
+        vec3 x0 = x - v0, x1 = x - v1, x2 = x - v2, x3 = x - v3;
+        vec4 hash = permute( permute( permute( 
+                    vec4(i0.z, i1.z, i2.z, i3.z ))
+                    + vec4(i0.y, i1.y, i2.y, i3.y ))
+                    + vec4(i0.x, i1.x, i2.x, i3.x ));
+        vec4 theta = hash * 3.883222077;
+        vec4 sz = hash * -0.006920415 + 0.996539792;
+        vec4 psi = hash * 0.108705628;
+        vec4 Ct = cos(theta), St = sin(theta);
+        vec4 sz_prime = sqrt( 1.0 - sz*sz );
+        vec4 gx, gy, gz;
+        if(alpha != 0.0) {
+            vec4 px = Ct * sz_prime, py = St * sz_prime, pz = sz;
+            vec4 Sp = sin(psi), Cp = cos(psi), Ctp = St*Sp - Ct*Cp;
+            vec4 qx = mix( Ctp*St, Sp, sz), qy = mix(-Ctp*Ct, Cp, sz);
+            vec4 qz = -(py*Cp + px*Sp);
+            vec4 Sa = vec4(sin(alpha)), Ca = vec4(cos(alpha));
+            gx = Ca*px + Sa*qx; gy = Ca*py + Sa*qy; gz = Ca*pz + Sa*qz;
         }
-      
-        vec4 permute(vec4 x)
-        {
-          return mod289(((x*34.0)+1.0)*x);
+        else {
+            gx = Ct * sz_prime; gy = St * sz_prime; gz = sz;  
         }
-      
-        vec4 taylorInvSqrt(vec4 r)
-        {
-          return 1.79284291400159 - 0.85373472095314 * r;
-        }
-      
-        vec3 fade(vec3 t) {
-          return t*t*t*(t*(t*6.0-15.0)+10.0);
-        }
-      
-        // Classic Perlin noise, periodic variant
-        float pnoise(vec3 P, vec3 rep)
-        {
-          vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
-          vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
-          Pi0 = mod289(Pi0);
-          Pi1 = mod289(Pi1);
-          vec3 Pf0 = fract(P); // Fractional part for interpolation
-          vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
-          vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
-          vec4 iy = vec4(Pi0.yy, Pi1.yy);
-          vec4 iz0 = Pi0.zzzz;
-          vec4 iz1 = Pi1.zzzz;
-      
-          vec4 ixy = permute(permute(ix) + iy);
-          vec4 ixy0 = permute(ixy + iz0);
-          vec4 ixy1 = permute(ixy + iz1);
-      
-          vec4 gx0 = ixy0 * (1.0 / 7.0);
-          vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
-          gx0 = fract(gx0);
-          vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
-          vec4 sz0 = step(gz0, vec4(0.0));
-          gx0 -= sz0 * (step(0.0, gx0) - 0.5);
-          gy0 -= sz0 * (step(0.0, gy0) - 0.5);
-      
-          vec4 gx1 = ixy1 * (1.0 / 7.0);
-          vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
-          gx1 = fract(gx1);
-          vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
-          vec4 sz1 = step(gz1, vec4(0.0));
-          gx1 -= sz1 * (step(0.0, gx1) - 0.5);
-          gy1 -= sz1 * (step(0.0, gy1) - 0.5);
-      
-          vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
-          vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
-          vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
-          vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
-          vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
-          vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
-          vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
-          vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
-      
-          vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
-          g000 *= norm0.x;
-          g010 *= norm0.y;
-          g100 *= norm0.z;
-          g110 *= norm0.w;
-          vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
-          g001 *= norm1.x;
-          g011 *= norm1.y;
-          g101 *= norm1.z;
-          g111 *= norm1.w;
-      
-          float n000 = dot(g000, Pf0);
-          float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
-          float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
-          float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
-          float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
-          float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
-          float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
-          float n111 = dot(g111, Pf1);
-      
-          vec3 fade_xyz = fade(Pf0);
-          vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
-          vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
-          float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
-          return 2.2 * n_xyz;
+        vec3 g0 = vec3(gx.x, gy.x, gz.x), g1 = vec3(gx.y, gy.y, gz.y);
+        vec3 g2 = vec3(gx.z, gy.z, gz.z), g3 = vec3(gx.w, gy.w, gz.w);
+        vec4 w = 0.5-vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3));
+        w = max(w, 0.0); vec4 w2 = w * w, w3 = w2 * w;
+        vec4 gdotx = vec4(dot(g0,x0), dot(g1,x1), dot(g2,x2), dot(g3,x3));
+        float n = dot(w3, gdotx);
+        vec4 dw = -6.0 * w2 * gdotx;
+        vec3 dn0 = w3.x * g0 + dw.x * x0;
+        vec3 dn1 = w3.y * g1 + dw.y * x1;
+        vec3 dn2 = w3.z * g2 + dw.z * x2;
+        vec3 dn3 = w3.w * g3 + dw.w * x3;
+        gradient = 39.5 * (dn0 + dn1 + dn2 + dn3);
+        return 39.5 * n;
         }`;
 
         const vertexShader = `  
@@ -1306,11 +1258,14 @@ class Play extends Phaser.Scene {
         ${noise}
         
         void main() {
-          float t = uTime;
-          float distortion = pnoise((normal + t) * uNoiseDensity, vec3(10.0)) * uNoiseStrength;
-          vec3 pos = position + (normal * distortion);
-          vNormal = normalMatrix * normal;
-          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
+            vec3 g;
+            float distortion = psrdnoise(1.5*position + uTime*vec3(0.1,0.123,0.134), uTime*10.0, g) * uNoiseStrength;
+            g *= 1.5; // scale gradient by inner derivative
+            vec3 pos = position + (normal * distortion);
+            vec3 nor_perp = g - dot(g, normal)*normal;
+            vec3 nor = normalize(normal - uNoiseStrength * nor_perp); // displace normal by gradient
+            vNormal = normalMatrix * nor;
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
         }`;
 
         const fragmentShader = `
@@ -1319,13 +1274,13 @@ class Play extends Phaser.Scene {
         uniform float uTime;
         
         void main() {
-          vec3 color = vec3(1.0);
-          vec3 normal = vNormal;
-          // ensure flipped normals look the same
-          if (normal.x + normal.y + normal.z < 0.0) {
-              normal = normal - 2.0*dot(normal, normalize(vec3(1.0,1.0,1.0)));
-          }
-          gl_FragColor = vec4(0.5+0.5*normalize(normal), 0.3);
+            vec3 color = vec3(1.0);
+            vec3 normal = vNormal;
+            // ensure flipped normals look the same
+            /*if (normal.x + normal.y + normal.z < 0.0) {
+                normal = normal - 2.0*dot(normal, normalize(vec3(1.0,1.0,1.0)));
+            }*/
+            gl_FragColor = vec4(0.5+0.5*normalize(normal), 0.3);
         }`;
 
         //this.three.tet_material = new THREE.MeshNormalMaterial({ transparent: true, opacity: 0.5, side: THREE.DoubleSide });
@@ -1341,7 +1296,6 @@ class Play extends Phaser.Scene {
             side: THREE.DoubleSide,
             transparent: true
         });
-        //console.log(this.tet_material);
 
         for (let i = 0; i < N + 1; i++) {
             let tet = new THREE.Mesh(tet_geom, this.three.tet_material);
@@ -2286,8 +2240,6 @@ class Play extends Phaser.Scene {
         }
         this.three.bounding_box.setFromObject(this.three.vertex_group);
         this.three.bounding_box.getBoundingSphere(this.three.bounding_sphere);
-        //v_smooth(this.three.camera_pos, this.three.bounding_sphere.center, delta * 0.005);
-        //this.three.camera.position.copy(this.three.camera_pos);
         this.three.camera.position.copy(this.three.bounding_sphere.center);
         this.three.camera.rotation.y += 0.0003 * delta;
         this.three.camera.position.x += 2 * this.three.bounding_sphere.radius * Math.sin(this.three.camera.rotation.y);
@@ -2295,7 +2247,6 @@ class Play extends Phaser.Scene {
         this.three.camera.far = this.three.bounding_sphere.radius * 10;
         this.three.camera.near = this.three.bounding_sphere.radius / 10;
         this.three.camera.updateProjectionMatrix();
-        //v_smooth(this.three.sphere_debug.position, this.three.bounding_sphere.center, delta * 0.01);
         this.three.sphere_debug.position.copy(this.three.bounding_sphere.center);
         this.three.sphere_debug.scale.setScalar(this.three.bounding_sphere.radius);
         for (let i = 0; i < N + 1; i++) {
