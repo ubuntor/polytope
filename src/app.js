e1af820e6324ec403fe7cf69dd0307c334771617
ubuntor <2906473+ubuntor@users.noreply.github.com>
2022-02-26 22:21:52 -0500

    wobbly

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.137.5';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js';
+import { mergeVertices } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/utils/BufferGeometryUtils.js';
+
 
 // blargh everything's using == and != but i don't want to go back and fix everything
 
@@ -84,6 +86,8 @@ const words = [
 ]
 words[3] = words[2];
 const NUM_TEXT_PARTICLES = 50;
+const VERTEX_COLORS = [0x7f3f3f, 0x727f3f, 0x3f7f59, 0x3f597f, 0x723f7f];
+const CAMERA_THUMP_ZOOM = 1.06;
 
 const background_canvas = document.createElement('canvas');
 document.body.appendChild(background_canvas);
@@ -134,7 +138,7 @@ class FinaleFX extends Phaser.Renderer.WebGL.Pipelines.PostFXPipeline {
             ]
         });
         this.saturation = 0;
-        // does this get constructed twice???
+        // does this get constructed an extra time by the game startup???
         console.log("created finale pipeline");
     }
     onPreRender() {
@@ -402,10 +406,9 @@ class Title extends Phaser.Scene {
             new THREE.Vector4(),
             new THREE.Vector4()
         ];
-        this.colors = [0x7f3f3f, 0x727f3f, 0x3f7f59, 0x3f597f, 0x723f7f];
         this.vertices_draw = [];
         for (let i = 0; i < 5; i++) {
-            this.vertices_draw.push(this.add.circle(0, 0, F(20), this.colors[i]));
+            this.vertices_draw.push(this.add.circle(0, 0, F(20), VERTEX_COLORS[i]));
         }
         this.theta = Math.random() * Math.PI * 2;
         this.r1 = new THREE.Matrix4();
@@ -498,7 +501,7 @@ class Title extends Phaser.Scene {
         this.background_graphics.lineStyle(5, 0x5f5f5f);
         for (let i = 0; i < 5; i++) {
             for (let j = i + 1; j < 5; j++) {
-                this.background_graphics.lineGradientStyle(5, this.colors[i], this.colors[j], this.colors[i], this.colors[j]);
+                this.background_graphics.lineGradientStyle(5, VERTEX_COLORS[i], VERTEX_COLORS[j], VERTEX_COLORS[i], VERTEX_COLORS[j]);
                 this.background_graphics.lineBetween(this.vertices_draw[i].x, this.vertices_draw[i].y, this.vertices_draw[j].x, this.vertices_draw[j].y);
             }
         }
@@ -762,13 +765,13 @@ class Play extends Phaser.Scene {
             this.text_particles[this.text_particle_index].tweens = [this.tweens.add({
                 targets: this.text_particles[this.text_particle_index],
                 scale: 0.25,
-                duration: 2000 / (1 + this.intensity),
+                duration: 2000 / (1 + this.intensity - (this.finale ? 0.5 : 0)),
                 ease: 'Quad.easeOut',
             }),
             this.tweens.add({
                 targets: this.text_particles[this.text_particle_index],
                 alpha: 0,
-                duration: 2000 / (1 + this.intensity),
+                duration: 2000 / (1 + this.intensity - (this.finale ? 0.5 : 0)),
                 ease: 'Expo.easeOut',
             })];
 
@@ -831,12 +834,12 @@ class Play extends Phaser.Scene {
                     if (this.camera_thump) {
                         this.camera_thump.remove();
                     }
-                    this.cameras.main.zoom = 1.03;
+                    this.cameras.main.zoom = CAMERA_THUMP_ZOOM;
                     this.camera_thump = this.tweens.add({
                         targets: this.cameras.main,
                         zoom: 1,
                         loop: -1,
-                        duration: 500,
+                        duration: 333,
                         ease: 'Sine.easeOut',
                     });
                     this.tweens.add({
@@ -1098,12 +1101,16 @@ class Play extends Phaser.Scene {
 
         this.three.vertices = [];
         this.three.vertex_mats = [];
-        this.three.vertex_color = new THREE.Color(0x333333);
+        this.three.vertex_colors = [];
         this.three.vertex_group = new THREE.Group();
         this.three.scene.add(this.three.vertex_group);
         let sphere_geom = new THREE.SphereGeometry(0.05, 32, 16);
+        let grey = new THREE.Color(0x333333);
         for (let i = 0; i < N + 1; i++) {
-            let material = new THREE.MeshPhongMaterial({ color: 0x333333, shininess: 100 });
+            let color = new THREE.Color(VERTEX_COLORS[i]);
+            color.lerp(grey, 0.7);
+            this.three.vertex_colors.push(color);
+            let material = new THREE.MeshPhongMaterial({ color, shininess: 100 });
             let vertex = new THREE.Mesh(sphere_geom, material);
             this.three.vertices.push(vertex);
             this.three.vertex_mats.push(material);
@@ -1135,12 +1142,171 @@ class Play extends Phaser.Scene {
         this.three.tets = [];
         this.three.tet_p = [];
         let tet_geom = gltf.scene.children[0].geometry;
-        tet_geom.applyMatrix4(new THREE.Matrix4().makeScale(1, -1, -1));
 
-        let tet_material = new THREE.MeshNormalMaterial({ transparent: true, opacity: 0.5 });
+        tet_geom.applyMatrix4(new THREE.Matrix4().makeScale(0.5, 0.5, 0.5));
+        tet_geom.deleteAttribute('normal');
+        tet_geom = mergeVertices(tet_geom);
+        tet_geom.computeVertexNormals();
+        console.log(tet_geom);
+
+        // modified from https://codepen.io/marioecg/pen/abmXKvW
+        const noise = `
+        // GLSL textureless classic 3D noise "cnoise",
+        // with an RSL-style periodic variant "pnoise".
+        // Author:  Stefan Gustavson (stefan.gustavson@liu.se)
+        // Version: 2011-10-11
+        //
+        // Many thanks to Ian McEwan of Ashima Arts for the
+        // ideas for permutation and gradient selection.
+        //
+        // Copyright (c) 2011 Stefan Gustavson. All rights reserved.
+        // Distributed under the MIT license. See LICENSE file.
+        // https://github.com/ashima/webgl-noise
+        //
+      
+        vec3 mod289(vec3 x)
+        {
+          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        }
+      
+        vec4 mod289(vec4 x)
+        {
+          return x - floor(x * (1.0 / 289.0)) * 289.0;
+        }
+      
+        vec4 permute(vec4 x)
+        {
+          return mod289(((x*34.0)+1.0)*x);
+        }
+      
+        vec4 taylorInvSqrt(vec4 r)
+        {
+          return 1.79284291400159 - 0.85373472095314 * r;
+        }
+      
+        vec3 fade(vec3 t) {
+          return t*t*t*(t*(t*6.0-15.0)+10.0);
+        }
+      
+        // Classic Perlin noise, periodic variant
+        float pnoise(vec3 P, vec3 rep)
+        {
+          vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+          vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+          Pi0 = mod289(Pi0);
+          Pi1 = mod289(Pi1);
+          vec3 Pf0 = fract(P); // Fractional part for interpolation
+          vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+          vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+          vec4 iy = vec4(Pi0.yy, Pi1.yy);
+          vec4 iz0 = Pi0.zzzz;
+          vec4 iz1 = Pi1.zzzz;
+      
+          vec4 ixy = permute(permute(ix) + iy);
+          vec4 ixy0 = permute(ixy + iz0);
+          vec4 ixy1 = permute(ixy + iz1);
+      
+          vec4 gx0 = ixy0 * (1.0 / 7.0);
+          vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+          gx0 = fract(gx0);
+          vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+          vec4 sz0 = step(gz0, vec4(0.0));
+          gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+          gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+      
+          vec4 gx1 = ixy1 * (1.0 / 7.0);
+          vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+          gx1 = fract(gx1);
+          vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+          vec4 sz1 = step(gz1, vec4(0.0));
+          gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+          gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+      
+          vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+          vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+          vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+          vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+          vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+          vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+          vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+          vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+      
+          vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+          g000 *= norm0.x;
+          g010 *= norm0.y;
+          g100 *= norm0.z;
+          g110 *= norm0.w;
+          vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+          g001 *= norm1.x;
+          g011 *= norm1.y;
+          g101 *= norm1.z;
+          g111 *= norm1.w;
+      
+          float n000 = dot(g000, Pf0);
+          float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+          float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+          float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+          float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+          float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+          float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+          float n111 = dot(g111, Pf1);
+      
+          vec3 fade_xyz = fade(Pf0);
+          vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+          vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+          float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+          return 2.2 * n_xyz;
+        }`;
+
+        const vertexShader = `  
+        varying vec3 vNormal;
+        
+        uniform float uTime;
+        uniform float uNoiseDensity;
+        uniform float uNoiseStrength;
+        
+        ${noise}
+        
+        void main() {
+          float t = uTime;
+          float distortion = pnoise((normal + t) * uNoiseDensity, vec3(10.0)) * uNoiseStrength;
+          vec3 pos = position + (normal * distortion);
+          vNormal = normalMatrix * normal;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
+        }`;
+
+        const fragmentShader = `
+        varying vec3 vNormal;
+        
+        uniform float uTime;
+        
+        void main() {
+          vec3 color = vec3(1.0);
+          vec3 normal = vNormal;
+          // ensure flipped normals look the same
+          if (normal.x + normal.y + normal.z < 0.0) {
+              normal = normal - 2.0*dot(normal, normalize(vec3(1.0,1.0,1.0)));
+          }
+          gl_FragColor = vec4(0.5+0.5*normalize(normal), 0.3);
+        }`;
+
+        //this.three.tet_material = new THREE.MeshNormalMaterial({ transparent: true, opacity: 0.5, side: THREE.DoubleSide });
+        this.three.tet_material = new THREE.ShaderMaterial({
+            vertexShader,
+            fragmentShader,
+            uniforms: {
+                uTime: { value: 0 },
+                uSpeed: { value: 4 },
+                uNoiseDensity: { value: 1.5 },
+                uNoiseStrength: { value: 0.1 },
+            },
+            side: THREE.DoubleSide,
+            transparent: true
+        });
+        //console.log(this.tet_material);
 
         for (let i = 0; i < N + 1; i++) {
-            let tet = new THREE.Mesh(tet_geom, tet_material);
+            let tet = new THREE.Mesh(tet_geom, this.three.tet_material);
             tet.matrixAutoUpdate = false;
             this.three.tets.push(tet);
             this.three.scene.add(tet);
@@ -1186,7 +1352,7 @@ class Play extends Phaser.Scene {
 
         this.text_particles = [];
         for (let i = 0; i < NUM_TEXT_PARTICLES; i++) {
-            this.text_particles.push(this.add.text(W / 2, H / 2, 'TEXT', { font: `${192 * FS}pt Covenant`, fill: '#ff9cf380', stroke: '#ff9cf3', strokeThickness: 4 }).setOrigin(0.5).setAlpha(0))
+            this.text_particles.push(this.add.text(W / 2, H / 2, 'TEXT', { font: `${F(192)}pt Covenant`, fill: '#ff9cf380', stroke: '#ff9cf3', strokeThickness: 4 }).setOrigin(0.5).setAlpha(0))
         }
 
         this.add.text(F(960), H - F(160), 'f1', { font: `${24 * FS}pt m3x6`, fill: '#f23af2' }).setOrigin(0.5);
@@ -1345,7 +1511,7 @@ class Play extends Phaser.Scene {
                     this.ending_rotations[i] += delta * 0.0005 * [7, 5, 3, 2][i];
                     let start = this.ending_rotations[i];
                     this.ending_graphics.beginPath();
-                    this.ending_graphics.arc(x, y, r, start, start + digits.length * width);
+                    this.ending_graphics.arc(x, y, F(r), start, start + digits.length * width);
                     this.ending_graphics.strokePath();
                     for (let j = 0; j < digits.length; j++) {
                         let cur_theta = start + width * (j + 0.5);
@@ -1454,9 +1620,12 @@ class Play extends Phaser.Scene {
             return;
         }
 
+        this.three.tet_material.uniforms.uNoiseStrength.value = 0.05 + 0.10 * this.intensity + (this.finale ? 0.15 : 0);
+        this.three.tet_material.uniforms.uTime.value += 0.001 * (0.2 + 2.8 * this.intensity + (this.finale ? 2 : 0)) * delta;
+
         if (!this.finale && !this.is_warning && this.intensity > 0.9) {
             this.is_warning = true;
-            this.cameras.main.zoom = 1.03;
+            this.cameras.main.zoom = CAMERA_THUMP_ZOOM;
             this.camera_thump = this.tweens.add({
                 targets: this.cameras.main,
                 zoom: 1,
@@ -2105,7 +2274,7 @@ class Play extends Phaser.Scene {
             }
         }
         for (let i = 0; i < N + 1; i++) {
-            this.three.vertex_mats[i].color.lerp(this.three.vertex_color, 0.005 * delta);
+            this.three.vertex_mats[i].color.lerp(this.three.vertex_colors[i], 0.005 * delta);
         }
         this.three.tets[0].matrix.makeBasis(this.three.line_vecs[1 * (N + 1) + 2], this.three.line_vecs[1 * (N + 1) + 3], this.three.line_vecs[1 * (N + 1) + 4]);
         this.three.tets[0].matrix.setPosition(this.three.vertices[1].position);
